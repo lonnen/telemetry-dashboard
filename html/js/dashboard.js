761dce39ae5ffe8b312b640aa505690c854a37ec
lonnen <chris.lonnen@gmail.com>
2013-09-09 14:03:33 -0700

    Partial documentation for dashboard.js.
    while debugging the regression caused by the 1.8 refactor I partially
    documented the functions in dashboard.js. The refactoring is light but
    some of the 1.8 idioms were replaced with standard iterators.

--- html/js/dashboard.js
+++ html/js/dashboard.js
@@ -1,6 +1,6 @@
 var selHistogram = document.getElementById("selHistogram");
 
-var _filter_set = Set();
+var _filter_set = {};
 
 function get(url, handler) {
     var xhr = new XMLHttpRequest();
@@ -16,19 +16,37 @@ function get(url, handler) {
     xhr.send(null);
 }
 
+/* removes all child elements of the parent element
+ *
+ * parent - a DOM node
+ *
+ * returns undefined
+ */
 function nukeChildren(parent) {
     while (parent.hasChildNodes()) {
         parent.removeChild(parent.lastChild);
     }
 }
 
+
+/* preprocess, filter, and plot all charts
+ *
+ * hgrams - an object containing the keys 'buckets' and 'values' representing
+ *          the data to plot
+ *
+ * returns undefined
+ */
 function drawChart(hgrams) {
     var i, data_, data;
     var FILTERID = 1;
     var ENTRY_COUNT = 2;
     var SUM = 3;
-    if (!hgrams) hgrams = window._hgrams;
-    if (!hgrams) return;
+    if (!hgrams) {
+        hgrams = window._hgrams;
+    }
+    if (!hgrams) {
+        return;
+    }
     window._hgrams = hgrams;
     var builds = Object.keys(hgrams.values).sort();
     var p50 = [];
@@ -40,8 +58,11 @@ function drawChart(hgrams) {
         var count = 0;
         for (data_ in hgrams.values[b]) {
             data = hgrams.values[b][data_];
+            console.log(data);
             filter = data[data.length - FILTERID];
-            if (!_filter_set.has(filter)) continue;
+            if (!_filter_set[filter]) {
+                continue;
+            }
             if (!total_histogram) {
                 total_histogram = data.slice();
                 continue;
@@ -72,7 +93,7 @@ function drawChart(hgrams) {
             for (data_ in hgrams.values[b]) {
                 data = hgrams.values[b][data_];
                 filter = data[data.length - FILTERID];
-                if (!_filter_set.has(filter)) {
+                if (!_filter_set[filter]) {
                     continue;
                 }
                 if (!tothistg) {
@@ -148,7 +169,9 @@ function drawChart(hgrams) {
     for (i = 0; i < hgrams.buckets.length; i++) {
         var x = hgrams.buckets[i];
         var y = total_histogram[i];
-        if (!y) continue;
+        if (!y) {
+            continue;
+        }
         barls.push([ i, y ]);
         ticks.push([ i, x ]);
         if (p50tick === null && x > p50) {
@@ -199,15 +222,26 @@ function estimatePercentile(buckets, values, percentile) {
 function updateDescription(descriptions) {
     var node = document.getElementById("divDescription");
     nukeChildren(node);
-    if (descriptions) window._descriptions = descriptions;
-    if (!window._descriptions) return;
+    if (descriptions) {
+        window._descriptions = descriptions;
+    }
+    if (!window._descriptions) {
+        return;
+    }
     var hgram = selHistogram.options[selHistogram.selectedIndex].value;
     var d = window._descriptions[hgram];
-    if (!d) return;
+    if (!d) {
+        return;
+    }
     var text = document.createTextNode(d.description);
     node.appendChild(text);
 }
 
+/* fetch data based on the current select box options, update the charts,
+ * description, and url based on the select box contents.
+ *
+ * return undefined
+ */
 function onhistogramchange() {
     var hgram = selHistogram.options[selHistogram.selectedIndex].value;
     get(window._path + "/" + hgram + ".json?", function() {
@@ -217,14 +251,22 @@ function onhistogramchange() {
     updateURL();
 }
 
+
+/* If the selection has not already been applied, decode the uri, and set the
+ * select boxes according to the URI.
+ *
+ * returns boolean indicating whether or not a selection was applied
+ */
 function applySelection() {
     if (window._appliedSelection) {
         return false;
     }
     window._appliedSelection = true;
-    var l = location.href;
-    var i = l.indexOf("#");
-    if (i == -1) return false;
+    var l = location.href,
+        i = l.indexOf("#");
+    if (i === -1) {
+        return false;
+    }
     var path = decodeURIComponent(l.substr(i + 1)).split("/");
     var parent = selHistogram.parentNode;
     var optionI = 0;
@@ -234,7 +276,9 @@ function applySelection() {
         var select = null;
         for (;optionI < parent.childNodes.length; optionI++) {
             select = parent.childNodes[optionI];
-            if (select.tagName == "SELECT") break;
+            if (select.tagName == "SELECT") {
+                break;
+            }
         }
         if (optionI == parent.childNodes.length) {
             console.log("Ran out of SELECTs in applySelection");
@@ -269,13 +313,20 @@ function applySelection() {
     return true;
 }
 
+/* If histograms and filters have loaded, populate histogram select box with
+ * options.
+ *
+ * returns undefined
+ */
 function stuffLoaded() {
-    if (!window._histograms || !window._filtersLoaded) return;
+    if (!window._histograms || !window._filtersLoaded) {
+        return;
+    }
     for (var h_ in window._histograms) {
-        var h = window._histograms[h_];
-        var o = document.createElement("option");
-        o.text = h;
-        selHistogram.add(o);
+        var histogram = window._histograms[h_],
+            option = document.createElement("option");
+        option.text = histogram;
+        selHistogram.add(option);
     }
     if (!applySelection()) {
         console.log("applySelection said there is nothing to do");
@@ -284,39 +335,71 @@ function stuffLoaded() {
     window._stuffLoaded = true;
 }
 
+/* Apply a whitelist to the histogram data and paint the charts
+ *
+ * filter - a tree like object of objects with leaves representing histogram
+ *          values to be allowed
+ *
+ * return undefined
+ */
 function applyFilter(filter) {
+    /* given an object of objects representing a tree, recursively fetch all
+     * leaves and add them to the set object
+     *
+     * tree - an object of objects
+     * set - an object with properties representing a set
+     *
+     * return the set object
+     */
     function getleafkeys(tree, set) {
         var id = tree["_id"];
-        if (id === undefined) return;
-        if (Object.keys(tree).length == 1) set.add(id);
+        if (id === undefined) {
+            return;
+        }
+        if (Object.keys(tree).length == 1) {
+            set[id] = true;
+        }
         for (var subtree_ in tree) {
             var subtree = tree[subtree_];
             getleafkeys(subtree, set);
         }
         return set;
     }
-    _filter_set = getleafkeys(filter, Set());
+    _filter_set = getleafkeys(filter, {});
     drawChart();
 }
 
 function updateURL() {
-    if (!window._stuffLoaded) return;
+    if (!window._stuffLoaded) {
+        return;
+    }
     var p = selHistogram.parentNode;
     var path = [];
     for (var i = 0; i < p.childNodes.length; i++) {
         var c = p.childNodes[i];
-        if (c.tagName != "SELECT") continue;
-        if (c.selectedIndex == -1) break;
+        if (c.tagName != "SELECT") {
+            continue;
+        }
+        if (c.selectedIndex == -1) {
+            break;
+        }
         path.push(c.options[c.selectedIndex].text);
     }
     location.href = "#" + path.join("/");
 }
 
+/* Change the whitelist filter applied to the histogram data and repaint the
+ * charts.
+ *
+ * returns undefined
+ */
 function filterChange() {
     var p = selHistogram.parentNode;
     for (var i = p.childNodes.length - 1; i > 0; i--) {
         var c = p.childNodes[i];
-        if (c == this) break;
+        if (c == this) {
+            break;
+        }
         p.removeChild(c);
     }
     updateURL();
@@ -326,33 +409,50 @@ function filterChange() {
     }
     next_filter_tree = this.filter_tree[this.options[this.selectedIndex].text];
     applyFilter(next_filter_tree);
-    if (next_filter_tree["name"]) initFilter(next_filter_tree);
+    if (next_filter_tree["name"]) {
+        initFilter(next_filter_tree);
+    }
 }
 
+/* Initialize data filter select boxes and set event listeners to filter data
+ * when an option is chosen.
+ *
+ * filter_tree - a whitelist object of objects representing the tree of
+ *               histogram values
+ *
+ * returns undefined
+ */
 function initFilter(filter_tree) {
     window._filtersLoaded = true;
-    var p = selHistogram.parentNode;
-    var s = document.createElement("select");
-    var o = document.createElement("option");
-    var id = filter_tree["_id"];
-    s.id = "selFilter" + id;
-    s.filter_tree = filter_tree;
-    o.text = filter_tree["name"] + " *";
-    s.add(o);
-    p.appendChild(s);
-    s.addEventListener("change", filterChange);
-    s.onChange = filterChange;
+    var parent = selHistogram.parentNode,
+        select = document.createElement("select"),
+        firstOption = document.createElement("option"),
+        id = filter_tree["_id"];
+    select.id = "selFilter" + id;
+    select.filter_tree = filter_tree;
+    firstOption.text = filter_tree["name"] + " *";
+    select.add(firstOption);
+    parent.appendChild(select);
+    select.addEventListener("change", filterChange);
+    select.onChange = filterChange;
     for (var opts in filter_tree) {
         if (opts == "_id" || opts == "name") {
             continue;
         }
         var option = document.createElement("option");
         option.text = opts;
-        s.add(option);
+        select.add(option);
     }
-    if (id === 0) filterChange.apply(s, [ true ]);
+    if (id === 0) {
+        filterChange.apply(select, [ true ]);
+    }
 }
 
+/* Reloads telemetry data specified into the page select boxes. Fetches
+ * supplemental data, and pickles selections to the url.
+ *
+ * returns undefined
+ */
 function loadData() {
     var selHistogram = document.getElementById("selHistogram");
     var selChannel = document.getElementById("selChannel");
@@ -385,6 +485,14 @@ function loadData() {
     });
 }
 
+/* Popluates select boxes with know telemetry data, populates the select boxes,
+ * sets them to either the URL specified values or defaults them to the latest
+ * nightly and sets event listeners to update the page when options are chosen.
+ *
+ * ls - an Array of "{releaseChannel/##}"
+ *
+ * returns undefined
+ */
 function buildVersionSelects(ls) {
     var urlChannel = /#([^/]+)/.exec(location.href),
         latestNightly = 0,
@@ -394,7 +502,9 @@ function buildVersionSelects(ls) {
         desiredChannel = decodeURIComponent(urlChannel[1]);
     }
     for (var i = 0; i < ls.length; i++) {
-        var chan = ls[i].split("/"), channel = chan[0], version = chan[1];
+        var chan = ls[i].split("/"),
+            channel = chan[0],
+            version = chan[1];
         if (channel == "nightly") {
             if (version > latestNightly) {
                 latestNightly = version;
@@ -416,16 +526,10 @@ function buildVersionSelects(ls) {
     loadData();
 }
 
-var plots = {};
-
 selHistogram.addEventListener("change", onhistogramchange);
-
 selHistogram.onChange = onhistogramchange;
 
-get("data/versions.json", function() {
-    buildVersionSelects(JSON.parse(this.responseText));
-});
-
+var plots = {};
 var resizeTimeout;
 
 function resizeAllPlots() {
@@ -465,3 +569,8 @@ for (var id_ in plotElements) {
     var id = plotElements[id_];
     $(id).bind("plothover", showPlotTooltip);
 }
+
+// kick everything off
+get("data/versions.json", function() {
+    buildVersionSelects(JSON.parse(this.responseText));
+});
